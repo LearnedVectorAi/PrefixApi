411419492c42f2be6401dc6e1bbc91b76a143636
LearnedVectorAi <85620103+LearnedVectorAi@users.noreply.github.com>
2021-06-09 17:01:03 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -45,7 +45,7 @@ while True:
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    if prob.item() > 1:
+    if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
